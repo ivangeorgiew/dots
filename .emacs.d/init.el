d77c2a6cd72c219f8fcf80ac2aef274cada6280d
Ivan Georgiev <ivan_georgiew@yahoo.com>
2021-05-28 18:06:33 +0300

    removed unnecessary files

--- .emacs.d/init.el
+++
@@ -1,4 +0,0 @@
-;;(setq my-global-map (make-keymap))
-;;(substitute-key-definition 'self-insert-command 'self-insert-command my-global-map global-map)
-;;(use-global-map my-global-map)
-(global-set-key (kbd "<C-.>") 'forward-char)
