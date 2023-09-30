9bd1fb8a9d93ecc73b943296b78dac50ee81d2c1
Ivan Georgiev <ivan_georgiew@yahoo.com>
2023-09-30 18:51:03 +0300

    added 2 aliases and moved nixpkgs file

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -1,4 +1,4 @@
 # Custom packages. Can be built using `nix build .#example`
-{ pkgs ? (import ./nixpkgs.nix) }: {
+{ pkgs ? (import ../nixpkgs.nix) }: {
   #example = pkgs.callPackage ./example { };
 }
