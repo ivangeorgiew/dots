3fd6bc8539f90dc0121f961b879729e08cb9f543
Ivan Georgiev <ivan_georgiew@yahoo.com>
2023-10-02 09:17:07 +0300

    setup hyprland

--- nix/modules/desktop.nix
+++ nix/modules/desktop.nix
@@ -1,6 +1,8 @@
 { inputs, outputs, lib, config, pkgs, ... }:
 let
   xrandrOpts = "--output DP-3 --primary --mode 1920x1080 --rate 240";
+  hyprlandPackage = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  xdgHyprlandPackage = inputs.xdg-desktop-portal-hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
 in
 {
   # Bad naming. Manages all the DE/WM settings, not only X11
@@ -24,18 +26,23 @@ in
     # DE/WM specific variables
     sessionVariables = {
       # Wayland specific variables
-      #NIXOS_OZONE_WL = "1";
-      #LIBVA_DRIVER_NAME = "nvidia";
-      #XDG_SESSION_TYPE = "wayland";
-      #WLR_NO_HARDWARE_CURSORS = "1";
-
-      # Disabled wayland variables, enable if necessary
       #GBM_BACKEND = "nvidia-drm"; # Could crash Firefox
+      #LIBVA_DRIVER_NAME = "nvidia";
       #__GLX_VENDOR_LIBRARY_NAME = "nvidia"; # Could cause issues with Discord and Zoom 
+      #__GL_GSYNC_ALLOWED = "1";
+      #__GL_VRR_ALLOWED = "1";
+      #NIXOS_OZONE_WL = "1";
+      #WLR_NO_HARDWARE_CURSORS = "1";
+      #XDG_SESSION_TYPE = "wayland";
+      #QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      #QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     };
 
     # Packages for DE/WM
     systemPackages = with pkgs; [
+      # hyprland related packages
+      #xdgHyprlandPackage    
+
       #bspwm
       #sxhkd
       #nitrogen
@@ -53,8 +60,9 @@ in
     hyprland = {
       enable = false;
 
-      # Substituted by the hyprland overlay
-      package = pkgs.hyprland;
+      # Hyprland's own flake package
+      #package = pkgs.hyprland;
+      package = hyprlandPackage;
       
       # Better nvidia support
       nvidiaPatches = true;      
