8b2fa3cae05212097b440f3c0c60dada7a28f9d3
Ivan Georgiev <ivan_georgiew@yahoo.com>
2023-10-25 14:24:12 +0300

    wip

--- nix/modules/environment.nix
+++ nix/modules/environment.nix
@@ -1,10 +1,13 @@
 # Env variables and aliases
 { inputs, outputs, lib, config, pkgs, username, ... }:
+let
+  browser = "brave-browser";
+in
 {
   environment = {
     sessionVariables = rec {
       TERMINAL = "kitty";
-      BROWSER = "brave-browser";
+      BROWSER = "${browser}";
       HISTCONTROL = "ignoreboth:erasedups";
       LESSHISTFILE = "-";
       PATH = [ XDG_BIN_HOME ];
@@ -27,19 +30,19 @@
       kl = "pkill -9"; # Force kill a process (hence the 9)
       ks = "ps aux | grep"; # List a process
       p = "pnpm"; # Launch pnpm node package manager
-      nix-update = "sudo nix flake update"
       nix-switch = "sudo nixos-rebuild switch --flake ~/dots/#"; # Change nixos config now
       nix-boot = "sudo nixos-rebuild boot --flake ~/dots/#"; # Change nixos config after boot
+      nix-update = "sudo nix flake update"; # Update the versions of packages
       nix-list = "sudo nix profile history --profile /nix/var/nix/profiles/system"; # List nixos generations
       nix-gc = "sudo nix profile wipe-history --profile /nix/var/nix/profiles/system --older-than 30d"; # Garbage collect nixos
     };
   };
 
   xdg.mime.defaultApplications = {
-    "text/html" = "brave-browser.desktop";
-    "x-scheme-handler/http" = "brave-browser.desktop";
-    "x-scheme-handler/https" = "brave-browser.desktop";
-    "x-scheme-handler/about" = "brave-browser.desktop";
-    "x-scheme-handler/unknown" = "brave-browser.desktop";
+    "text/html" = "${browser}.desktop";
+    "x-scheme-handler/http" = "${browser}.desktop";
+    "x-scheme-handler/https" = "${browser}.desktop";
+    "x-scheme-handler/about" = "${browser}.desktop";
+    "x-scheme-handler/unknown" = "${browser}.desktop";
   };
 }
