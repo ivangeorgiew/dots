5e0d3eb3bcc17ba40c838d7ce51cd8b2a3acbd9b
Ivan Georgiev <ivan_georgiew@yahoo.com>
2023-12-06 22:07:10 +0200

    nvim and some other minor changes

--- links/.config/nvim/lua/config/options.lua
+++ links/.config/nvim/lua/config/options.lua
@@ -1,6 +1,10 @@
 -- see `:help vim.o` for more
 local o = vim.opt
 
+o.tabstop = 2 -- amount of spaces on tab
+o.shiftwidth = 2 -- amount of spaces on shifting
+o.expandtab = true -- change tab to use spaces
+
 o.autowriteall = true -- smart auto-save on some commands
 o.backup = false -- toggle backup file
 o.breakindent = true -- wrapped line continues on the same indent level
@@ -9,7 +13,6 @@ o.complete = ".,t" -- where to get completions from
 o.completeopt = "menu,menuone,noselect,longest" -- completion options
 o.confirm = true -- popup to save file after some commands
 o.diffopt = "vertical,iwhite,filler" -- vimdiff split direction and ignore whitespace
-o.expandtab = true -- change tab to use spaces
 o.foldlevel = 0 -- close all folds by default
 o.foldmethod = "marker" -- default fold method
 o.formatoptions = "tcrqlj" -- formatting options
@@ -26,7 +29,6 @@ o.ph = 10 -- max entries in a popup
 o.redrawtime = 5000 -- increase redraw time for syntax handling
 o.relativenumber = true -- relative line numbers
 o.scrolloff = 10 -- min lines below and above
-o.shiftwidth = 2 -- amount of spaces on shifting
 o.shortmess:append({ W = true, I = true, c = true, C = true }) -- skip some unnecessary messages
 o.showcmd = false -- whether to show partial command on last line
 o.showmode = false -- don't show vim typing mode
@@ -36,7 +38,6 @@ o.splitbelow = true -- new windows are below
 o.splitkeep = "screen" -- keep text on the same screen line when resizing
 o.splitright = true -- new windows are to the right
 o.swapfile = false -- toggle swapfile
-o.tabstop = 2 -- amount of spaces on tab
 o.tagstack = false -- don't add tags manually
 o.termguicolors = true -- enables 24-bit RGB colors
 o.textwidth = 0 -- max line char length
@@ -47,5 +48,5 @@ o.updatetime = 500 -- writes swap file to disk every X ms
 o.virtualedit = "block" -- be able to place the cursor anywhere during vis block mode
 o.wildmode = "longest:full,full" -- command line completion
 o.winminwidth = 5 -- min width of a window
-o.wrap = true -- line wrap
+o.wrap = false -- line wrap
 o.showbreak = " ⤷ " -- arrow for wrapped text
