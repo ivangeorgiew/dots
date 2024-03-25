868d30e59943f2bbcd2beae15d701ad511b19d6f
Ivan Georgiev <ivan_georgiew@yahoo.com>
2024-03-25 10:37:21 +0200

    nvim config small refactor

--- links/.config/nvim/init.lua
+++ links/.config/nvim/init.lua
@@ -1,13 +1,13 @@
 --local config_files = vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/config", [[v:val =~ "\.lua$"]])
 
-local tie_up = require("tie_up")
+local tie = require("utils").tie
 
-tie_up("require config files", {}, function()
+tie("require config files", {}, function()
   local ordered_configs = { "options", "plugins", "autocmds", "keymaps" }
 
   for _, file in ipairs(ordered_configs) do
     local descr = "requiring " .. file .. ".lua"
 
-    tie_up(descr, {}, function() require("config/" .. file:gsub("%.lua$", "")) end)()
+    tie(descr, {}, function() require("config/" .. file:gsub("%.lua$", "")) end)()
   end
 end)()
