4dc8204f61f94a406aecbf1d9691584aae085d1a
Ivan Georgiev <ivan_georgiew@yahoo.com>
2024-03-30 12:05:59 +0200

    nvim small refactor

--- links/.config/nvim/lua/config/plugins.lua
+++ links/.config/nvim/lua/config/plugins.lua
@@ -1,11 +1,16 @@
 -- setup plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local uv = vim.uv or vim.loop
 
-if not vim.loop.fs_stat(lazypath) then
+if not uv.fs_stat(lazypath) then
+  print("Installing lazy.nvim...")
+
   vim.fn.system({
     "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
   })
+
+  print("Done.")
 end
 
 vim.opt.rtp:prepend(lazypath)
