ac2a5bdbebba529f4b06a0ca63c60a43941b6d27
Ivan Georgiev <ivan_georgiew@yahoo.com>
2024-04-15 20:31:51 +0300

    nix: packages.nix and environment.nix changes
    - merged environment.nix and packages.nix
    - added config for npm global packages in the fish init script

--- nix/modules/environment.nix
+++ nix/modules/environment.nix
@@ -1,12 +1,72 @@
 { inputs, outputs, lib, config, pkgs, username, ... }:
 {
   environment = {
+    systemPackages = with pkgs; [
+      # CLI apps and tools
+      btop # system monitor
+      cava # audio visualizer
+      cmatrix # cool effect
+      curl # download files
+      dash # fastest shell
+      fd # better alternative to find
+      ffmpeg # for audio and video
+      fishPlugins.colored-man-pages
+      fishPlugins.done # get notification when long process finishes
+      fswatch # file change monitor required by some programs
+      fzf # fuzzy file searcher
+      gcc # c compiler
+      gh # github authenticator
+      git # obvious
+      glibc # need by some programs
+      gnumake # make command
+      jq # json processor
+      killall # kill a running process
+      lazygit # terminal UI for git commands
+      libvterm-neovim # needed by neovim
+      nerdfix # removes obsolete nerd font icons
+      nitch # alternative to neofetch
+      p7zip # archiving and compression
+      pavucontrol # audio control
+      pstree # prints tree of pids
+      ripgrep # newest silver searcher + grep
+      shared-mime-info # add new custom mime types (check arch wiki)
+      stow # symlink dotfiles
+      tree-sitter # needed by neovim
+      unzip # required by some programs
+      wget # download files
+
+      # GUI apps
+      brave # browser
+      celluloid # mpv with GUI (video player)
+      firefox-bin # browser
+      floorp # browser
+      gedit # basic text editor GUI
+      google-chrome # browser
+      keepassxc # password manager
+      kitty # terminal
+      libsForQt5.ark # 7-zip alternative
+      mpv # video player
+      nur.repos.nltch.spotify-adblock # spotify
+      qbittorrent # torrent downloading
+      vesktop # discord + additions
+      viber # chat app
+
+      #Lua
+      lua
+
+      # Python
+      (python310.withPackages(ps: with ps; [ requests pygobject3 ]))
+      gobject-introspection # for some python scripts
+
+      # Javascript
+      nodejs
+    ];
+
     sessionVariables = rec {
       TERMINAL = "kitty";
       BROWSER = "floorp";
       HISTCONTROL = "ignoreboth:erasedups";
       LESSHISTFILE = "-";
-      PATH = [ XDG_BIN_HOME ];
       XDG_CONFIG_HOME = "$HOME/.config";
       XDG_CACHE_HOME = "$HOME/.cache";
       XDG_DATA_HOME = "$HOME/.local/share";
@@ -37,6 +97,71 @@
     };
   };
 
+  # Config/add packages
+  programs = {
+    # Interactive shell
+    fish = {
+      enable = true;
+
+      useBabelfish = true; # Bash to Fish translation
+
+      interactiveShellInit =
+      let
+        npm_global_dir = "~/.npm-global";
+        npm_packages = "npm pnpm neovim tailwindcss typescript";
+      in ''
+        #Disable greeting
+        set fish_greeting
+
+        # Display system info
+        nitch
+
+        # set the path for npm global packages
+        npm set prefix ${npm_global_dir}
+
+        # add the npm globals to PATH
+        fish_add_path --path ${npm_global_dir}/bin
+
+        # install npm global packages
+        nohup npm i -g ${npm_packages} </dev/null &>/dev/null &
+
+        # Start in the projects folder
+        if not test -d ~/projects
+          mkdir ~/projects
+        end
+
+        cd ~/projects
+      '';
+
+      #loginShellInit =
+      #let
+      #  dquote = str: "\"" + str + "\"";
+      #  makeBinPathList = map (path: path + "/bin");
+      #in ''
+      #  # Fix for/from https://github.com/LnL7/nix-darwin/issues/122#issuecomment-1659465635
+      #  fish_add_path --move --prepend --path ${lib.concatMapStringsSep " " dquote (makeBinPathList config.environment.profiles)}
+      #  set fish_user_paths $fish_user_paths
+      #'';
+    };
+
+    # shell prompt
+    starship.enable = true;
+
+    # IDE/Text editor
+    neovim = {
+      enable = true;
+      package = pkgs.neovim; # overlayed neovim-nightly
+      withRuby = true;
+      withPython3 = true;
+      withNodeJs = true;
+
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+  };
+
+  # Associate programs with file extensions
   xdg.mime =
     # list from /run/current-system/sw/share/applications
     let
